eb5db48787805541aa3f928925140fe03cbc4cf5
Steven Borrelli <steve@borrelli.org>
2024-05-21 00:46:35 -0500

    initial commit
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

---
+++ render.sh
@@ -0,0 +1,6 @@
+crossplane beta render \
+  --extra-resources examples/extra-resources \
+  --include-full-xr \
+  --include-context \
+  examples/xdocdb.yaml apis/xdocdb/composition.yaml examples/functions.yaml 
+
